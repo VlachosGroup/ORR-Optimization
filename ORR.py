0104ffddcc384a4d27ee4d1072e5d4e876c2c35e
mpnunez <mpnunez@udel.edu>
2017-11-23 12:42:00 -0500

    ORR uses referencing

--- ORR.py
+++ ORR.py
@@ -29,19 +29,46 @@ def ORR_rate(delEads_OH, delEads_OOH):
     # *OH, *OOH
     E_g = [-7.53, -13.26]               # energies of OH(g) and OOH(g)
     ZPE = [0.332, 0.428]                # zero-point energy correction, eV
-    TS = [0, 0]                         # entropy contribution to Gibbs energyat 298 K, eV
+    TS = [0, 0]                         # entropy contribution to Gibbs energy at 298 K, eV
     E_solv = [-0.575, -0.480]           # solvation energy, eV
     
     # Species free energies at T = 298K
     G_OH = E_g[0] + delEads_OH + ZPE[0] - TS[0] + E_solv[0]
     G_OOH = E_g[1] + delEads_OOH + ZPE[1] - TS[1] + E_solv[1]
-    G_H2g = -6.8935
-    G_H2Ol = -14.3182
-    G_O2g = -9.9294            # gas thermo obtained from ../DFT_data/Volcano_rederive.m
     
-    # Compute G1 and G4
-    G1 = G_OOH - G_O2g - 0.5 * G_H2g + U * n
-    G4 = G_H2Ol - G_OH - 0.5 * G_H2g + U * n
+    # Gas species Gibbs energies
+    # H2(g), H2O(l), O2(g), OH(g), OOH(g)
+    E_DFT_gas = [-6.7595, -14.2222, -9.86]             # From my own DFT data
+    
+    
+    # H2, H2O(l)
+    ZPE_gas = [0.270, 0.574]             # eV, 
+    TS_gas = [0.404, 0.583]              # at 298 K, eV / K
+    E_solv_gas = [0, -0.087]             # eV
+    
+    G_H2g = E_DFT_gas[0] + ZPE_gas[0] - TS_gas[0] + E_solv_gas[0]
+    G_H2Ol = E_DFT_gas[1] + ZPE_gas[1] - TS_gas[1] + E_solv_gas[1]
+    G_O2g = 2 * (G_H2Ol - G_H2g) + 4 * U_0
+    
+    # These are the values you should get for gas phase Gibbs energies
+    # G_H2g = -6.8935
+    # G_H2Ol = -14.3182
+    # G_O2g = -9.9294
+    
+    # Compute G1 and G4 - without referencing
+    #G1 = G_OOH - G_O2g - 0.5 * G_H2g
+    #G4 = G_H2Ol - G_OH - 0.5 * G_H2g
+    
+    # Compute G1 and G4 - with referencing to H2(g) and H2O(l)
+    delta_G_OH = G_OH + 0.5 * G_H2g - G_H2Ol
+    delta_G_OOH = G_OOH + 1.5 * G_H2g - 2*G_H2Ol
+    delta_G_O2 = 4 * U_0
+    G1 = delta_G_OOH - delta_G_O2
+    G4 = - delta_G_OH
+    
+    # Add contribution from cell potential
+    G1 += U * n
+    G4 += U * n
     
     delG_max = max(G1,G4)
     j = i_c * np.exp( - delG_max  / (kB * T) )
