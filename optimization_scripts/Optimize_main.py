9497d01682f41262f09de43251d3c3d282174aa3
mpnunez <mpnunez@udel.edu>
2018-03-01 23:45:29 -0500

    Analyzed optimization results

--- optimization_scripts/Optimize_main.py
+++ optimization_scripts/Optimize_main.py
@@ -5,23 +5,50 @@ Stage 1: Maximize current density and minimize surface energy simultaneously
 Stage 2: Minimize surface energy by moving atoms to adjacent locations at constant loading
 '''
 
+import sys
+sys.path.append('/home/vlachos/mpnunez/Github/ORR-Optimization')
+
 import os
 import numpy as np
+import random
 from orr_cat import orr_cat
 from sim_anneal import *
 
 import matplotlib.pyplot as plt
 import matplotlib as mat
 
-data_fldr = '/home/vlachos/mpnunez/ORR_data/Josh_MKM/other'
-#data_fldr = os.path.expanduser(data_fldr)
-MOO_weight = 1.0
+if len(sys.argv) > 1:
+    MOO_weight = float(sys.argv[1]) / 1000.0 # divide by total number of jobs
+    MOO_weight = MOO_weight ** 2
+    data_fldr = str(np.int(sys.argv[1]) + 1000)
+    random.seed(a=int(sys.argv[1]))
+else:
+    MOO_weight = 1.0
+    data_fldr = 'solo_run'
+
+# Make folder for new structure
+if os.path.exists(data_fldr ):
+    # Clear folder contents
+    for the_file in os.listdir(data_fldr ):
+        file_path = os.path.join(data_fldr , the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+else:
+    os.makedirs(data_fldr )
+
+print MOO_weight
+np.save(os.path.join(data_fldr, 'omega.npy'), MOO_weight)
 
 '''
 Build catalyst structure
 '''
 
-cat = orr_cat(dim1 = 15, dim2 = 15)
+cat = orr_cat(dim1 = 20, dim2 = 20, volcano = 'CV')
 cat.randomize(build_structure = True)
 
 # Compute normalization factors
@@ -37,7 +64,7 @@ Multiobjective optimization (stage 1)
 '''
 
 # Optimize
-traj_hist_a = optimize(cat, weight = MOO_weight, ensemble = 'GCE', n_cycles = 25, T_0 = 1.2, 
+traj_hist_a = optimize(cat, weight = MOO_weight, ensemble = 'GCE', n_cycles = 500, T_0 = 1.2, 
     j_norm = I_norm, se_norm = E_form_norm, n_record = 100, verbose = True)
 np.save(os.path.join(data_fldr, 'trajectory_a.npy'), traj_hist_a)
 
@@ -73,8 +100,6 @@ plt.close()
 cat.occs_to_atoms()
 cat.show(fname = os.path.join(data_fldr, 'optimized'), fmat = 'png', transmute_top = True)
 cat.show(fname = os.path.join(data_fldr, 'optimized'), fmat = 'xsd', transmute_top = True)
-
-raise NameError('stop')
 
 '''
 Surface energy minimization to meta-stable structure (stage 2)
---
+++ optimization_scripts/find_omega.py
@@ -0,0 +1,32 @@
+'''
+Main script for optimizing ORR catalyst structure
+Two stage optimization
+Stage 1: Maximize current density and minimize surface energy simultaneously
+Stage 2: Minimize surface energy by moving atoms to adjacent locations at constant loading
+'''
+
+import sys
+sys.path.append('/home/vlachos/mpnunez/Github/ORR-Optimization')
+
+import os
+import numpy as np
+import random
+from orr_cat import orr_cat
+from sim_anneal import *
+
+import matplotlib.pyplot as plt
+import matplotlib as mat
+
+fldr = '.'
+
+# List all folders
+subfldr_list = []
+for fldr_or_file in os.listdir(fldr):
+    if os.path.isdir(os.path.join(fldr,fldr_or_file)):
+        subfldr_list.append(fldr_or_file)
+        
+
+for subfldr in subfldr_list:
+    ind = np.float(subfldr)
+    omega = ind / 1000.0
+    np.save(os.path.join(subfldr, 'omega.npy'), omega)
